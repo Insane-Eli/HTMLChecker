30c71bdbfc410a21e6e7576c5b0ce63766d33cfc
Eli <150296902+Insane-Eli@users.noreply.github.com>
2025-05-21 12:44:22 -0700

    commented methods + removed extra file

--- HTMLChecker/HTMLManager.java
+++ HTMLChecker/HTMLManager.java
@@ -4,10 +4,10 @@ public class HTMLManager {
   private Queue<HTMLTag> tags;
   
   public HTMLManager(Queue<HTMLTag> html){
-   if(html == null){
-      throw new IllegalArgumentException();
+   if(html == null){ 
+      throw new IllegalArgumentException(); // make sure html isn't null
    } else {
-      tags = html;
+      tags = html; // set our tags field to the input html
    }
   }
   
@@ -17,10 +17,10 @@ public class HTMLManager {
   
   public String toString(){
    String str = "";
-   int size = tags.size();
-   for(int i = 0; i < size; i++){
+   int size = tags.size(); // get the size of our queue
+   for(int i = 0; i < size; i++){ // loop through the queue
       HTMLTag temp = tags.remove();
-      str += temp.toString().trim();
+      str += temp.toString().trim(); // add the elements to the string
       tags.add(temp);
    }
    return str;
@@ -35,10 +35,10 @@ public class HTMLManager {
    
       HTMLTag temp = tags.remove(); // take the tag out of the queue
       
-      if(temp.isOpening()){
-         stack.add(temp);
-         tags.add(temp);         
-      } else if (temp.isClosing() && !stack.empty()){ // closer
+      if(temp.isOpening()){ // if it's an opener
+         stack.add(temp); // add it to the stack
+         tags.add(temp); // add it to the queue   
+      } else if (temp.isClosing() && !stack.empty()){ // if it's a closer
          
          if(temp.matches(stack.peek())){ // if the closer matches the opener at the top of the stack
             stack.pop(); // remove the tag from the stack
@@ -48,13 +48,13 @@ public class HTMLManager {
          }
          
       } else if (temp.isSelfClosing()){
-         tags.add(temp);
+         tags.add(temp); // if it's self closing, add it to the queue no questions asked
       }
     }
     
-    size = stack.size();
+    size = stack.size(); //for the remaining openers in the stack
     for(int i = 0; i < size; i++){
-      tags.add(stack.pop().getMatching());
+      tags.add(stack.pop().getMatching()); // add closers for them
     }
    }
 
