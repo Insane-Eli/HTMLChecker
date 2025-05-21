77e1b5f6557581d79539e75df30d498fd1cb7ce7
Eli <150296902+Insane-Eli@users.noreply.github.com>
2025-05-21 12:22:10 -0700

    completed fixHTML()

--- HTMLChecker/HTMLManager.java
+++ HTMLChecker/HTMLManager.java
@@ -7,7 +7,7 @@ public class HTMLManager {
    if(html == null){
       throw new IllegalArgumentException();
    } else {
-      this.tags = html;
+      tags = html;
    }
   }
   
@@ -20,14 +20,42 @@ public class HTMLManager {
    int size = tags.size();
    for(int i = 0; i < size; i++){
       HTMLTag temp = tags.remove();
-      str += temp;
+      str += temp.toString().trim();
       tags.add(temp);
    }
    return str;
   }
 
-  void fixHTML(){
+  public void fixHTML(){
+  
+   // make a stack
+   Stack<HTMLTag> stack = new Stack<HTMLTag>();   
+   int size = tags.size();
+   for(int i = 0; i < size; i++){ // go through every tag in the queue
    
-  }
+      HTMLTag temp = tags.remove(); // take the tag out of the queue
+      
+      if(temp.isOpening()){
+         stack.add(temp);
+         tags.add(temp);         
+      } else if (temp.isClosing() && !stack.empty()){ // closer
+         
+         if(temp.matches(stack.peek())){ // if the closer matches the opener at the top of the stack
+            stack.pop(); // remove the tag from the stack
+            tags.add(temp); // add the closer to the queue
+         } else { //if it doesn't
+            tags.add(stack.pop().getMatching()); // add the closer yourself and pop the stack
+         }
+         
+      } else if (temp.isSelfClosing()){
+         tags.add(temp);
+      }
+    }
+    
+    size = stack.size();
+    for(int i = 0; i < size; i++){
+      tags.add(stack.pop().getMatching());
+    }
+   }
 
 }
