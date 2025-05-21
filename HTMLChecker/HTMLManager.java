a3ccdc8ab96b34209dc0043d5a82cea852c581cc
Eli <150296902+Insane-Eli@users.noreply.github.com>
2025-05-21 11:12:38 -0700

    added constructor, getTags, and toString

--- HTMLChecker/HTMLManager.java
+++ HTMLChecker/HTMLManager.java
@@ -3,4 +3,31 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
+  public HTMLManager(Queue<HTMLTag> html){
+   if(html == null){
+      throw new IllegalArgumentException();
+   } else {
+      this.tags = html;
+   }
+  }
+  
+  public Queue<HTMLTag> getTags(){
+   return tags;  
+  }
+  
+  public String toString(){
+   String str = "";
+   int size = tags.size();
+   for(int i = 0; i < size; i++){
+      HTMLTag temp = tags.remove();
+      str += temp;
+      tags.add(temp);
+   }
+   return str;
+  }
+
+  void fixHTML(){
+   
+  }
+
 }
